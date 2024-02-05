54da88f954b0e3c9f1584d041e6a21df2f7fc05c
sairu-sh <tmg.sairush@gmail.com>
2024-02-05 20:09:00 +0545

    finished the static view of the packer app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>💼Far Away 🌴</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list and you already packed X(x%)</em>
+    </footer>
+  );
+}
